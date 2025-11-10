6489a572a88d67da20c81d820e3a7f834d35dbe0
Mohamed Morsy <mohamedmorsy477@gmail.com>
2025-11-10 16:39:52 +0200

    working on deployment

--- frontend/src/pages/AddDoctor.js
+++ frontend/src/pages/AddDoctor.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from "react-router-dom";
-//import { useAuth } from "../context/authContext.js";
 
 const AddDoctor = () => {
   const [name, setName] = useState("");
